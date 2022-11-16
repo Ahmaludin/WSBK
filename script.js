cc0de28f3dc136521f2a9c8ebf9d1f74bdd99b34
Ahmaludin <ahmal54321@gmail.com>
2022-11-16 14:28:09 +0700

    fix info standings, superpole, bestlap

--- script.js
+++ script.js
@@ -95,11 +95,39 @@ articles.addEventListener("click", function (e) {
   // End of - sistem perubahan warna menu pada info
 
   // sistem perubahan rider pole
-  function addRider(flag, riderName, riderTeam, riderPoint) {
-    return `<span class="riderName"><span class="fi fi-${flag}"></span> ${riderName}</span>
+  function addRider(flag, riderName, riderTeam) {
+    return (
+      riderPoint
+    ) => `<span class="riderName"><span class="fi fi-${flag}"></span> ${riderName}</span>
     <span class="riderTeam">${riderTeam}</span>
     <span class="riderPoint">${riderPoint}</span>`;
   }
+
+  // Rider list
+  const dominiqueAegerter = addRider("ch", "DOMINIQUE AEGERTER", "yamaha");
+  const lorenzoBaldassarri = addRider("it", "LORENZO BALDASSARRI", "yamaha");
+  const canOncu = addRider("tr", "CAN ONCU", "kawasaki");
+  const nicoloBulega = addRider("it", "NICOLO BULEGA", "ducati");
+  const stefanoManzi = addRider("it", "STEFANO MANZI", "triumph");
+  const federicoCaricasulo = addRider("it", "FEDERICO CARICASULO", "ducati");
+  const alvaroDiaz = addRider("es", "ALVARO DIAZ", "yamaha");
+  const victorSteeman = addRider("nl", "VICTOR STEEMAN", "kawasaki");
+  const hugoDeCancellis = addRider("fr", "HUGO DE CANCELLIS", "kawasaki");
+  const marcGarcia = addRider("es", "MARC GARCIA", "yamaha");
+  const samuelDiSora = addRider("fr", "SAMUEL DI SORA", "kawasaki");
+  const mirkoGennai = addRider("it", "MIRKO GENNAI", "yamaha");
+  const alvaroBautista = addRider("es", "ALVARO BAUTISTA", "DUCATI");
+  const toprakRazgatlioglu = addRider("tr", "TOPRAK RAZGATLIOGLU", "yamaha");
+  const jonathanRea = addRider("gb", "JONATHAN REA", "kawasaki");
+  const michaelRubenRinaldi = addRider("it", "MICHAEL RUBEN RINALDI", "ducati");
+  const andreaLocatelli = addRider("it", "ANDREA LOCATELLI", "yamaha");
+  const alexLowes = addRider("gb", "ALEX LOWES", "kawasaki");
+  const inigoIglesias = addRider("es", "INIGO IGLESIAS", "kawasaki");
+  const dirkGeiger = addRider("de", "DIRK GEIGER", "kawasaki");
+  const kevinSabatucci = addRider("it", "KEVIN SABATUCCI", "kawasaki");
+  const raffaeleDeRosa = addRider("it", "RAFFAELE DE ROSA", "ducati");
+  const matteoVannucci = addRider("it", "MATTEO VANUCCI", "yamaha");
+  // End of - Rider list
 
   // standings
   const standingsRPL = Array.from(
@@ -110,99 +138,89 @@ articles.addEventListener("click", function (e) {
 
   // standings ssp
   if (articleTCN == "ssp" && standingsCN == "standings info") {
-    standingsRPL[0].innerHTML = addRider(
-      "ch",
-      "DOMINIQUE AEGERTER",
-      "yamaha",
-      "462"
-    );
-    standingsRPL[1].innerHTML = addRider(
-      "it",
-      "LORENZO BALDASSARRI",
-      "yamaha",
-      "359"
-    );
-    standingsRPL[2].innerHTML = addRider("tr", "CAN ONCU", "kawasaki", "248");
-    standingsRPL[3].innerHTML = addRider(
-      "it",
-      "NICOLO BULEGA",
-      "ducati",
-      "210"
-    );
-    standingsRPL[4].innerHTML = addRider(
-      "it",
-      "STEFANO MANZI",
-      "triumph",
-      "207"
-    );
-    standingsRPL[5].innerHTML = addRider(
-      "it",
-      "FEDERICO CARICASULO",
-      "ducati",
-      "191"
-    );
+    standingsRPL[0].innerHTML = dominiqueAegerter(462);
+    standingsRPL[1].innerHTML = lorenzoBaldassarri(359);
+    standingsRPL[2].innerHTML = canOncu(248);
+    standingsRPL[3].innerHTML = nicoloBulega(210);
+    standingsRPL[4].innerHTML = stefanoManzi(207);
+    standingsRPL[5].innerHTML = federicoCaricasulo(191);
   }
   // standings ssp300
   else if (articleTCN == "ssp300" && standingsCN == "standings info") {
-    standingsRPL[0].innerHTML = addRider("es", "ALVARO DIAZ", "yamaha", "259");
-    standingsRPL[1].innerHTML = addRider(
-      "nl",
-      "VICTOR STEEMAN",
-      "kawasaki",
-      "180"
-    );
-    standingsRPL[2].innerHTML = addRider(
-      "fr",
-      "HUGO DE CANCELLIS",
-      "kawasaki",
-      "171"
-    );
-    standingsRPL[3].innerHTML = addRider("es", "MARC GARCIA", "yamaha", "164");
-    standingsRPL[4].innerHTML = addRider(
-      "fr",
-      "SAMUEL DI SORA",
-      "kawasaki",
-      "146"
-    );
-    standingsRPL[5].innerHTML = addRider("it", "MIRKO GENNAI", "yamaha", "140");
+    standingsRPL[0].innerHTML = alvaroDiaz(259);
+    standingsRPL[1].innerHTML = victorSteeman(180);
+    standingsRPL[2].innerHTML = hugoDeCancellis(171);
+    standingsRPL[3].innerHTML = marcGarcia(164);
+    standingsRPL[4].innerHTML = samuelDiSora(146);
+    standingsRPL[5].innerHTML = mirkoGennai(140);
   }
   // standings sbk
   else if (articleTCN == "sbk" && standingsCN == "standings info") {
-    standingsRPL[0].innerHTML = addRider(
-      "es",
-      "ALVARO BAUTISTA",
-      "DUCATI",
-      "553"
-    );
-    standingsRPL[1].innerHTML = addRider(
-      "tr",
-      "TOPRAK RAZGATLIOGLU",
-      "yamaha",
-      "487"
-    );
-    standingsRPL[2].innerHTML = addRider(
-      "gb",
-      "JONATHAN REA",
-      "kawasaki",
-      "450"
-    );
-    standingsRPL[3].innerHTML = addRider(
-      "it",
-      "MICHAEL RUBEN RINALDI",
-      "ducati",
-      "279"
-    );
-    standingsRPL[4].innerHTML = addRider(
-      "it",
-      "ANDREA LOCATELLI",
-      "yamaha",
-      "245"
-    );
-    standingsRPL[5].innerHTML = addRider("gb", "ALEX LOWES", "kawasaki", "234");
+    standingsRPL[0].innerHTML = alvaroBautista(553);
+    standingsRPL[1].innerHTML = toprakRazgatlioglu(487);
+    standingsRPL[2].innerHTML = jonathanRea(450);
+    standingsRPL[3].innerHTML = michaelRubenRinaldi(279);
+    standingsRPL[4].innerHTML = andreaLocatelli(245);
+    standingsRPL[5].innerHTML = alexLowes(234);
   }
 
   // superpole
+  const superpoleRPL = Array.from(
+    document.querySelectorAll(".superpole .riderPoleList")
+  );
+  const superpoleCN =
+    e.target.parentElement.parentElement.parentElement.className;
+
+  // superpole ssp
+  if (articleTCN == "ssp" && superpoleCN == "superpole info") {
+    superpoleRPL[0].innerHTML = dominiqueAegerter(111);
+    superpoleRPL[1].innerHTML = federicoCaricasulo(74);
+    superpoleRPL[2].innerHTML = lorenzoBaldassarri(70);
+    superpoleRPL[3].innerHTML = nicoloBulega(65);
+    superpoleRPL[4].innerHTML = canOncu(48);
+  }
+  // superpole ssp300
+  else if (articleTCN == "ssp300" && superpoleCN == "superpole info") {
+    superpoleRPL[0].innerHTML = victorSteeman(61);
+    superpoleRPL[1].innerHTML = inigoIglesias(33);
+    superpoleRPL[2].innerHTML = marcGarcia(32);
+    superpoleRPL[3].innerHTML = dirkGeiger(31);
+    superpoleRPL[4].innerHTML = kevinSabatucci(28);
+  }
+  // superpole sbk
+  else if (articleTCN == "sbk" && superpoleCN == "superpole info") {
+    superpoleRPL[0].innerHTML = toprakRazgatlioglu(217);
+    superpoleRPL[1].innerHTML = jonathanRea(203);
+    superpoleRPL[2].innerHTML = alvaroBautista(166);
+    superpoleRPL[3].innerHTML = alexLowes(105);
+    superpoleRPL[4].innerHTML = andreaLocatelli(76);
+  }
+
   // bestlap
+  const bestLapRPL = Array.from(
+    document.querySelectorAll(".bestLap .riderPoleList")
+  );
+  const bestLapCN =
+    e.target.parentElement.parentElement.parentElement.className;
+
+  // bestLap ssp
+  if (articleTCN == "ssp" && bestLapCN == "bestLap info") {
+    bestLapRPL[0].innerHTML = dominiqueAegerter(12);
+    bestLapRPL[1].innerHTML = lorenzoBaldassarri(4);
+    bestLapRPL[2].innerHTML = raffaeleDeRosa(2);
+  }
+  // bestLap ssp300
+  else if (articleTCN == "ssp300" && bestLapCN == "bestLap info") {
+    bestLapRPL[0].innerHTML = samuelDiSora(3);
+    bestLapRPL[1].innerHTML = victorSteeman(3);
+    bestLapRPL[2].innerHTML = matteoVannucci(3);
+  }
+  // bestLap sbk
+  else if (articleTCN == "sbk" && bestLapCN == "bestLap info") {
+    bestLapRPL[0].innerHTML = toprakRazgatlioglu(8);
+    bestLapRPL[1].innerHTML = alvaroBautista(8);
+    bestLapRPL[2].innerHTML = jonathanRea(5);
+  }
   // End of - sistem perubahan rider pole
 
   // ---------- End of - Info content
@@ -210,3 +228,43 @@ articles.addEventListener("click", function (e) {
 // End of - Articles click event
 
 // -------------------- End of - ARTICLES
+
+// let ahmaludin = {
+//   flag: "id",
+//   rider: this,
+//   team: "kawasaki",
+// };
+
+// function addRider(rider, riderPoint) {
+//   `<span class="riderName"><span class="fi fi-${rider.flag}"></span> ${rider.rider}</span>
+//     <span class="riderTeam">${rider.team}</span>
+//     <span class="riderPoint">${rider.point}</span>`;
+// }
+
+// console.log(addRider("ahmaludin", "10"));
+//
+// function ucapkanSalam(waktu) {
+//   return function (nama) {
+//     console.log(`hi ${nama}, selamat ${waktu}`);
+//   };
+// }
+
+// let pagi = ucapkanSalam("pagi");
+// pagi("amal");
+
+//
+
+// const standingsRPL = document.querySelector(".standings .riderPoleList");
+
+// function addRider(flag, riderName, riderTeam) {
+//   return (
+//     riderPoint
+//   ) => `<span class="riderName"><span class="fi fi-${flag}"></span> ${riderName}</span>
+//   <span class="riderTeam">${riderTeam}</span>
+//   <span class="riderPoint">${riderPoint}</span>`;
+// }
+
+// let ahmaludin = addRider("id", "Ahmaludin", "kawasaki");
+// // ahmaludin("10");
+
+// standingsRPL.innerHTML = ahmaludin("10");
